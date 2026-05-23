3c852203c050bccd2c7c4c84e505131b297bddfc
Yolol100 <Andrew_Baeten@hotmail.com>
2026-05-24 05:45:02 +0700

    test

--- blocks/language-switcher/editor.asset.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (! defined('ABSPATH')) {
-    exit;
-}
-
-return [
-    'dependencies' => ['wp-blocks', 'wp-element', 'wp-i18n', 'wp-server-side-render'],
-    'version' => '1.6.5',
-];
--- build/admin/index.asset.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (! defined('ABSPATH')) {
-    exit;
-}
-
-return array('dependencies' => array('wp-api-fetch','wp-components','wp-element','wp-i18n','wp-notices'), 'version' => '1.6.5');
